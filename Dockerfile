37237d6d27dc3ceeea8f36b9e4fcc750d21524c8
matheusbsilva <matheusbattista@hotmail.com>
2018-06-14 10:05:22 -0300

    Create dockerfile and docker-compose

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.6
+
+WORKDIR /app
+
+ADD requirements.txt /app/
+
+RUN pip install -r requirements.txt
+
+ADD . /app/
+
+CMD ["./run.sh"]
+
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Set development to APP_ENVIRONMENT if it is unset 
 export FLASK_ENV=${APP_ENVIRONMENT:=development}
 export FLASK_APP=pingout
-flask run
+flask run --host=0.0.0.0
