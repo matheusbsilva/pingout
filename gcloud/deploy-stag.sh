4ad48ae15b0ef4c850aa12c794d75e83cf18375e
matheusbsilva <matheusbattista@hotmail.com>
2018-09-07 17:40:44 -0300

    Add ci/cd config files

---
+++ gcloud/deploy-stag.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+docker build -t gcr.io/${PROJECT_ID}/${PINGOUT_IMAGE}:$TRAVIS_COMMIT .
+
+# Turn key to service auth json
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_ID
+gcloud --quiet config set container/cluster $CLUSTER 
+gcloud --quiet config set compute/zone ${ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER
+
+gcloud docker -- push gcr.io/${PROJECT_ID}/${PINGOUT_IMAGE}
+
+kubectl set image deployment/${PINGOUT_DEPLOYMENT} ${PINGOUT_CONTAINER}=gcr.io/${PROJECT_ID}/${PINGOUT_IMAGE}:$TRAVIS_COMMIT
